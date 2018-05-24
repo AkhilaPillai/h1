88b9f9c9d12658f222753f5f33ade3db1d017dd3
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-05-24 15:39:43 +0530

    Create max.cpp

---
+++ max.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n[50],i,m;
+   for(i=0;i<10;i++)
+   {
+       cin>>n[i];
+   }
+m=n[0];
+for(i=0;i<10;i++)
+{
+    if(m<n[i])
+    {
+        m=n[i];
+    }
+}
+cout<<m;
+    return 0;
+}
