03e1215faa3549d2dedade01f7d69a76c6b8f16d
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-05-24 14:26:40 +0530

    Create spaceless.cpp

---
+++ spaceless.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    int i,c=0;
+    getline(cin, str);
+    for(i=0;i<str.length();i++)
+    {
+    if(str.at(i)==' ')
+    {
+        c++;
+    }
+}
+cout<<c;
+   return 0;
+}
